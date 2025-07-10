c925c3593d6ee81e0c40fa255b48fb725624e504
jp-roisin <jeanpaul.roisin@ptrotonmail.com>
2025-07-10 22:58:21 +0200

    [SQLC 001] Generate sessions CRUD

---
+++ internal/database/queries/sessions.sql
@@ -0,0 +1,19 @@
+-- name: GetSession :one
+SELECT * FROM sessions
+WHERE id = ? LIMIT 1;
+
+-- name: ListSessions :many
+SELECT * FROM sessions
+ORDER BY created_at DESC;
+
+-- name: CreateSession :one
+INSERT INTO sessions (
+    id
+) VALUES (
+    ?
+)
+RETURNING *;
+
+-- name: DeleteSession :exec
+DELETE from sessions
+WHERE id = ?;
