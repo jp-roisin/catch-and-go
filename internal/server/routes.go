08fb2a16f7a01bc0b464866797cd63c2324472b7
jp-roisin <jeanpaul.roisin@protonmail.com>
2025-07-11 00:00:30 +0200

    Add KeyAuthWithConfig middleware

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -2,8 +2,10 @@ package server
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/a-h/templ"
+	"github.com/google/uuid"
 	"github.com/jp-roisin/catch-and-go/cmd/web"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -13,6 +15,41 @@ func (s *Server) RegisterRoutes() http.Handler {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(middleware.KeyAuthWithConfig(middleware.KeyAuthConfig{
+		KeyLookup: `cookie:"token"`,
+		Validator: func(key string, c echo.Context) (bool, error) {
+			ctx := c.Request().Context()
+
+			// TODO: Cache tokens in memory to avoid hitting the DB on every request.
+			// A simple in-memory map might be enough, or consider using an LRU cache:
+			// https://github.com/hashicorp/golang-lru
+			_, err := s.db.GetSession(ctx, key)
+			if err != nil {
+				return false, c.String(http.StatusUnauthorized, "Session token is not valid")
+			}
+			return true, nil
+		},
+		ErrorHandler: func(err error, c echo.Context) error {
+			ctx := c.Request().Context()
+
+			session, err := s.db.CreateSession(ctx, uuid.New().String())
+			if err != nil {
+				return c.String(http.StatusInternalServerError, "Couldn't create a session token")
+			}
+
+			c.SetCookie(
+				&http.Cookie{
+					Name:     "token",
+					Value:    session.ID,
+					Path:     "/",
+					HttpOnly: true,
+					Expires:  time.Now().Add(365 * 24 * time.Hour),
+				},
+			)
+
+			return nil
+		},
+	}))
 
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins:     []string{"https://*", "http://*"},
