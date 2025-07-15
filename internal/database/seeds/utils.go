690a1b22e097a85a22bd2fa3a516278279fd8521
jp-roisin <jeanpaul.roisin@ptrotonmail.com>
2025-07-15 19:05:51 +0200

    Add IO seeds for lines

---
+++ internal/database/seeds/utils.go
@@ -0,0 +1,37 @@
+package seeds
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+type i18nCell struct {
+	Fr string `json:"fr"`
+	Nl string `json:"nl"`
+}
+
+var validString = regexp.MustCompile(`^[a-zA-Z0-9]+$`)
+
+const batchSize = 100
+
+func readCsvFile(filePath string) ([][]string, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	csvReader.Comma = ';'
+	csvReader.LazyQuotes = true
+
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return records, nil
+}
